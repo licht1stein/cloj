fdf5caa472a4f8df6a6056d20d349c9e20dbd0f1
MB <mb@blaster.ai>
2021-01-27 23:38:04 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Clojure inspired helper functions
+
+This is a small library with Clojure inspired helper functions. It has no external dependencies.
+
+The goal is to implement replicas of the Clojure funcs that I find useful.
+
+Implemented:
+* take/drop
+* some
+* nth, first, second, third, fourth, fifth, forty_second, last
+
+Some examples:
+
+```python
+from cloj import take, drop
+
+take(3, [1, 2, 3, 4, 5])
+>> [1, 2, 3]
+
+drop(3, [1, 2, 3, 4, 5])
+>> [4, 5]
+
+drop (100, [1, 2, 3, 4, 5])
+>> []
+```
+
+```python
+from cloj import first, Fsecond, third, fourth, fifth, forty_second, last
+
+first([1, 2, 3])
+>> 1
+
+last([1, 2, 3])
+>> 3
+
+first([])
+>> None
+```
---
+++ cloj/funcs.py
@@ -0,0 +1,31 @@
+import functools
+from collections import Callable, Iterable, Sized
+from contextlib import suppress
+
+
+def some(fn: Callable, collection: Iterable):
+    for i in collection:
+        if result := fn(i):
+            return result
+
+
+def nth(i: int, collection: Sized):
+    with suppress(IndexError):
+        return collection[i]
+
+
+first = functools.partial(nth, 0)
+second = functools.partial(nth, 1)
+third = functools.partial(nth, 2)
+fourth = functools.partial(nth, 3)
+fifth = functools.partial(nth, 4)
+forty_second = functools.partial(nth, 41)
+last = functools.partial(nth, -1)
+
+
+def take(n, coll):
+    return coll[:n]
+
+
+def drop(n, coll):
+    return coll[n:]
